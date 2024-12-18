dac4e7400e7a882cc3dc9deef749d3be6f083b1e
Eliaquim da Luz <eliaquim.daluz@gmail.com>
2024-12-18 23:45:43 +0000

    008

--- app/Livewire/Contacts.php
+++ app/Livewire/Contacts.php
@@ -2,10 +2,19 @@
 
 namespace App\Livewire;
 
+use App\Models\Contact;
+use Illuminate\Support\Collection;
 use Livewire\Component;
 
 class Contacts extends Component
 {
+    public Collection $contacts;
+
+    public function mount()
+    {
+        $this->contacts = Contact::all();
+    }
+    
     public function render()
     {
         return view('livewire.contacts');
