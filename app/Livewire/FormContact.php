9d0f9d7446d64fbd63dad52fcabd661f7483463d
Eliaquim da Luz <eliaquim.daluz@gmail.com>
2024-12-18 22:54:42 +0000

    005

--- app/Livewire/FormContact.php
+++ app/Livewire/FormContact.php
@@ -2,32 +2,43 @@
 
 namespace App\Livewire;
 
+use App\Models\Contact;
 use Illuminate\Support\Facades\Log;
 use Illuminate\View\View;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class FormContact extends Component
 {
-    public $name, $email, $phone;
+    #[Validate('required|min:3|max:50')]
+    public string $name;
+
+    #[Validate('required|email|min:5|max:50')]
+    public string $email;
+
+    #[Validate('required|min:6|max:20')]
+    public string $phone;
 
     public function newContact()
     {
         //validate
-        $this->validate(
+        $this->validate();
+        
+        // temporary store in log file
+        //Log::info('Novo contacto: ' . $this->name . ' - ' . $this->email . ' - ' . $this->phone);
+
+        // Store contact in database
+        Contact::firstOrCreate(
             [
-                'name' => 'required|min:3|max:50',
-                'email' => 'required|email|min:5|max:50',
-                'phone' => 'required|min:6|max:20'
+                'name' => $this->name,
+                'email' => $this->email
+            ],
+            [
+                'phone' => $this->phone
             ]
         );
 
-        // temporary store in log file
-        Log::info('Novo contacto: ' . $this->name . ' - ' . $this->email . ' - ' . $this->phone);
-
         // Clear form
-        // $this->name = '';
-        // $this->email = '';
-        // $this->phone = '';
         $this->reset();
     }
     
